5ec8d1eb3b6f8665565e5e227ad69a7fa31183af
longhoanglekim <115090953+longhoanglekim@users.noreply.github.com>
2024-11-25 23:32:52 +0700

    add getFlightList api in userController

--- src/main/java/com/web/airplane/demo/models/Flight.java
+++ src/main/java/com/web/airplane/demo/models/Flight.java
@@ -1,5 +1,6 @@
 package com.web.airplane.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -27,15 +28,19 @@ public class Flight {
     private Airport destinationAirport;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime expectedDepartureTime;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime expectedArrivalTime;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime actualDepartureTime;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime actualArrivalTime;
 
     @ManyToOne
