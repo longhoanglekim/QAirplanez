002264e540196f0d262a96ac82d1727a44a623a6
Bang <thbang727@gmail.com>
2024-11-27 00:43:01 +0700

    add tailwind x n

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,54 +1,61 @@
 <template>
-  <h1>aa</h1>
-  <div id="app">
-    <header><PageHeader/></header>
-    <PageNavBar/>
+<h1 class="text-2xl font-bold underline">2xl</h1>
+<h1 class="text-3xl font-bold underline">3xl</h1>
+<div id="app">
+    <header>
+        <PageHeader />
+    </header>
+    <PageNavBar />
     <router-view></router-view>
-    <footer><PageFooter/></footer>
-  </div>
-
+    <footer>
+        <PageFooter />
+    </footer>
+</div>
 </template>
 
 <script>
-
 import PageHeader from "@/components/PageHeader.vue";
 import PageFooter from "@/components/PageFooter.vue";
 import PageNavBar from "./components/PageNavBar.vue";
 
 export default {
-  name: 'App',
-  components: {PageFooter, PageHeader, PageNavBar},
+    name: 'App',
+    components: {
+        PageFooter,
+        PageHeader,
+        PageNavBar
+    },
 
-  data() {
-    return {
-    };
-  }
+    data() {
+        return {};
+    }
 }
 </script>
 
 <style>
-body, html {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  width: 100%;
-  font-family: Arial, sans-serif;
-  /* display: flex;
+body,
+html {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+    font-family: Arial, sans-serif;
+    /* display: flex;
   justify-content: center;
   align-items: center; */
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-}
-footer {
-  font-family: "Rubik", sans-serif;
-  background-color: rgb(255, 242, 242);
-  margin-top: 0;
-  padding-top: 1px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
 }
 
+footer {
+    font-family: "Rubik", sans-serif;
+    background-color: rgb(255, 242, 242);
+    margin-top: 0;
+    padding-top: 1px;
+}
 </style>
--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "../router";
-import "./assets/style/tailwind.css";
+import "./tailwind.css";
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
