1906c8ba0f194cd58ef86f781b7c8c32f55da8be
longhoanglekim <115090953+longhoanglekim@users.noreply.github.com>
2024-12-01 16:49:16 +0700

    add favicon gothrough

--- src/main/java/com/web/airplane/demo/configs/JwtAuthenticationFilter.java
+++ src/main/java/com/web/airplane/demo/configs/JwtAuthenticationFilter.java
@@ -72,7 +72,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String path = request.getRequestURI();
         List<String> publicPaths = Arrays.asList("/api/auth/", "/api/ticket_class/", "/api/flight/public/",
                 "/test/");
-        return path.contains("/public/") || publicPaths.stream().anyMatch(path::startsWith);
+        return path.contains("/public/") || path.contains("/favicon.ico")
+        || publicPaths.stream().anyMatch(path::startsWith);
     }
 
 }
