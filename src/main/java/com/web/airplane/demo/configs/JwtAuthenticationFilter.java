4360e3a8ccbcf1ad74b821e2917d2435fed5128f
longhoanglekim <115090953+longhoanglekim@users.noreply.github.com>
2025-01-26 21:25:36 +0700

    transit bean

--- src/main/java/com/web/airplane/demo/configs/JwtAuthenticationFilter.java
+++ src/main/java/com/web/airplane/demo/configs/JwtAuthenticationFilter.java
@@ -1,26 +1,17 @@
 package com.web.airplane.demo.configs;
 
-        import com.web.airplane.demo.models.Role;
-        import com.web.airplane.demo.models.User;
-        import com.web.airplane.demo.repositories.UserRepository;
         import com.web.airplane.demo.services.JwtService;
-        import com.web.airplane.demo.utils.JwtUtil;
-        import io.jsonwebtoken.ExpiredJwtException;
         import jakarta.servlet.FilterChain;
         import jakarta.servlet.ServletException;
-        import jakarta.servlet.http.Cookie;
         import jakarta.servlet.http.HttpServletRequest;
         import jakarta.servlet.http.HttpServletResponse;
         import lombok.extern.slf4j.Slf4j;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.lang.NonNull;
         import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
         import org.springframework.security.core.context.SecurityContextHolder;
         import org.springframework.security.core.userdetails.UserDetails;
         import org.springframework.security.core.userdetails.UserDetailsService;
         import org.springframework.stereotype.Component;
         import org.springframework.web.filter.OncePerRequestFilter;
-
         import java.io.IOException;
         import java.util.Arrays;
         import java.util.List;
@@ -31,11 +22,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
-    @Autowired
-    private UserRepository userRepository;
-    private final List<String> publicUrls = List.of(
-
-    );
 
     public JwtAuthenticationFilter(
             JwtService jwtService,
--- src/main/java/com/web/airplane/demo/configs/SecurityConfiguration.java
+++ src/main/java/com/web/airplane/demo/configs/SecurityConfiguration.java
@@ -1,28 +1,14 @@
 package com.web.airplane.demo.configs;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.expression.SecurityExpressionHandler;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -30,24 +16,22 @@ public class SecurityConfiguration {
 
     private final AuthenticationProvider authenticationProvider;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-
-    @Autowired
-    private RoleHierarchy roleHierarchy;
-
+    private final CorsConfigurationSource corsConfigurationSource;
 
     public SecurityConfiguration(
             JwtAuthenticationFilter jwtAuthenticationFilter,
-            AuthenticationProvider authenticationProvider
-    ) {
+            AuthenticationProvider authenticationProvider,
+            CorsConfigurationSource corsConfigurationSource) {
         this.authenticationProvider = authenticationProvider;
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.corsConfigurationSource = corsConfigurationSource;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // Disable CSRF protection
-                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Enable CORS
+                .cors(cors -> cors.configurationSource(corsConfigurationSource)) // Enable CORS
                 .exceptionHandling(exceptions -> exceptions.accessDeniedPage("/access-denied"))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("api/flight/admin/**", "/test/admin/**").hasRole("ADMIN")
@@ -67,16 +51,6 @@ public class SecurityConfiguration {
         return http.build();
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:3000"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        configuration.setAllowCredentials(true);
-        return source;
-    }
+
 
 }
